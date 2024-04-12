80f4357628780a0703fa805b07f69fdc46e48f26
HUJOE003 <hpandise@calpoly.edu>
2024-04-12 13:23:05 -0700

    first commit

---
+++ packages/react-frontend/src/MyApp.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from "react";
+import Table from "./Table";
+import Form from "./Form";
+
+
+function MyApp() {
+    <Form handleSubmit={updateList} />
+    const [characters, setCharacters] = useState([]);
+
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
+  return (
+    <div className="container">
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+      />
+      <Form />
+    </div>
+  );
+}
+
+export default MyApp;
